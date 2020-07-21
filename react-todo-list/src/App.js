b14748cd7d3aba2ceb80168281389efc829b565b
tamago1509 <54825087+tamago1509@users.noreply.github.com>
2020-07-21 17:55:04 +0700

    create todolis by creating react app

---
+++ react-todo-list/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>Go to market</li>
+        <li>Buy food</li>
+        <li>Cook dinner</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
