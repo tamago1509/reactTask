95691842c7b97f6daf50c2c6158d1081107d62a6
tamago1509 <54825087+tamago1509@users.noreply.github.com>
2020-07-27 22:03:56 +0700

    create protype for user

--- new-todolist/src/App.js
+++ new-todolist/src/App.js
@@ -1,65 +1,31 @@
 import React, { Component } from 'react'
 
-
 import './App.css';
-import TodoList from './components/todo-list/TodoList'
-import TodoItem from './components/todo-item/TodoItem'
+import FollowUserListItem from './components/FollowUserListItem'
 
 class App extends Component {
   constructor(props) {
     super(props)
   
     this.state = {
-       list : []
+       user: [
+         {
+         nick: 'phong9080',
+         avatarURL: 'https://picsum.photos/50'
+       },
+       {
+        nick: 'phong0908',
+        avatarURL: 'https://picsum.photos/50'
+       }
+      ]
     }
   }
-  
-  componentDidMount() {
-    let list = JSON.parse(localStorage.getItem("Todolist")) || []
-    this.setState({
-      list
-    })
-  }
-  
-
 
   render() {
+    const {user} = this.state;
     return (
       <div className="App">
-        <h3>Dailist</h3>
-        <TodoList type="upcoming">
-          {
-            this.state.list.map((item, index) => {
-              if(!item.isComplete){
-                return (
-                  <TodoItem  
-                    id={index}
-                    title={item.title}
-                    isComplete={item.isComplete}
-                  />
-                )
-              }
-            })
-          }
-        </TodoList>
-
-        {/* finish */}
-        <TodoList type="finish">
-          {
-            this.state.list.map((item, index) => {
-              if(item.isComplete){
-                return (
-                  <TodoItem  
-                    id={index}
-                    title={item.title}
-                    isComplete={item.isComplete}
-                  />
-                )
-              }
-            })
-          }
-        </TodoList>
-        <button>+</button>
+        <FollowUserListItem user = {user}/>
       </div>
     )
   }
--- new-todolist/src/components/FollowUserListItem.jsx
+++ new-todolist/src/components/FollowUserListItem.jsx
@@ -1,22 +1,34 @@
 import React, { Component } from 'react'
 import './FollowUserListItem.css'
+import PropTypes from 'prop-types';
 
-export class FollowUserListItem extends Component {
-
-
+class FollowUserListItem extends Component {
+    
     render() {
         let {user} = this.props;
         return (
-            <div>
-                <img src="https://picsum.photos/50" alt="avatar"/>
-                <div>
-                    <span>{user.nick}</span>
-                    <span>Gợi ý cho bạn</span>
-                </div>
-                <span><a href="#">Theo dõi</a></span>
+            <div >
+            {
+                user.map((user,key) =>
+                <div className="friend" key ={key}>
+                        <img src={user.avatarURL} alt="avatar"/>
+                    <div className= 'content'>
+                        <span><strong>{user.nick}</strong></span><br/>
+                        <span className="sub">Gợi ý cho bạn</span>
+                    </div>
+                    <span><a href="#">Theo dõi</a></span>
+                </div>)  
+            }
             </div>
         )
     }
 }
+    FollowUserListItem.propTypes = {
+        user: PropTypes.arrayOf(PropTypes.shape({
+            nick: PropTypes.string,
+            avatarURL: PropTypes.string
+        }))
+    
+    }
 
 export default FollowUserListItem
